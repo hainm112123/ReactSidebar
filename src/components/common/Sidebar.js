144f47a338828547c38b67c49e3d3fec48c72a4c
yuuhi <hainm112123@gmail.com>
2023-06-16 10:50:06 +0700

    sidebar ui

--- src/components/common/Sidebar.js
+++ src/components/common/Sidebar.js
@@ -1,10 +1,52 @@
 import React from 'react'
+import { Avatar, Drawer, List, ListItemButton, ListItemIcon, Stack, Toolbar} from '@mui/material'
 
-const Props = {};
+import sizeConfigs from '../../configs/sizeConfigs'
+import assets from '../../assets/index'
+import colorConfigs from '../../configs/colorConfigs';
+import appRoutes from '../../routes/appRoutes';
 
-const Sidebar = (props = Props) => {
+const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <Drawer
+      variant="permanent"
+      sx={{
+        width: sizeConfigs.sidebar.width,
+        flexShrink: 0,
+        '& .MuiDrawer-paper': {
+          width: sizeConfigs.sidebar.width,
+          boxSizing: 'border-box',
+          borderRight: "0px",
+          backgroundColor: colorConfigs.sidebar.bg,
+          color: colorConfigs.sidebar.color,
+        },
+      }}
+    > 
+      <List disablePadding>
+        <Toolbar>
+          <Stack
+            sx={{width: "100%"}}
+            direction="row"
+            justifyContent="center"
+          > 
+            <Avatar src={assets.images.logo} />
+          </Stack>
+        </Toolbar>
+
+        {appRoutes.map((route, index) => (
+          route.sidebarProps ? (
+            <ListItemButton>
+              <ListItemIcon sx={{
+                color: colorConfigs.sidebar.color
+              }}>
+                {route.sidebarProps.icon && route.sidebarProps.icon}
+              </ListItemIcon>
+              {route.sidebarProps.displayText}
+            </ListItemButton>
+          ) : null
+        ))}
+      </List>
+    </Drawer>
   );
 };
 
--- src/routes/appRoutes.js
+++ src/routes/appRoutes.js
@@ -1,15 +1,32 @@
 import DashboardOutlinedIcon from '@mui/icons-material/DashboardOutlined';
+import AppsOutlinedIcon from '@mui/icons-material/AppsOutlined';
+import ListOutlinedIcon from '@mui/icons-material/ListOutlined';
+import FileDownloadOutlinedIcon from '@mui/icons-material/FileDownloadOutlined';
+import ArticleOutlinedIcon from '@mui/icons-material/ArticleOutlined';
 
 import HomePage from "../pages/home/HomePage"
 import DashboardPageLayout from "../pages/dashboard/DashboardPageLayout"
 import DefaultPage from "../pages/dashboard/DefaultPage"
 import DashboardIndex from '../pages/dashboard/DashboardIndex';
+import ComponentPageLayout from '../pages/components/ComponentPageLayout';
+import ChangelogPage from '../pages/changelog/ChangelogPage';
+import InstallationPage from '../pages/installation/InstallationPage';
+import DocumentationPage from '../pages/documentation/DocumentationPage';
 
 const appRoutes = [
   {
     index: true,
     element: <HomePage />,
     state: "home"
+  },
+  {
+    path: "/installation",
+    element: <InstallationPage />,
+    state: "installation",
+    sidebarProps: {
+      displayText: "Installation",
+      icon: <FileDownloadOutlinedIcon />
+    }
   },
   {
     path: "/dashboard",
@@ -34,6 +51,33 @@ const appRoutes = [
         }
       }
     ],
+  },
+  {
+    path: "/components",
+    element: <ComponentPageLayout />,
+    state: "components",
+    sidebarProps: {
+      displayText: "Components",
+      icon: <AppsOutlinedIcon />
+    }
+  },
+  {
+    path: "/documentation",
+    element: <DocumentationPage />,
+    state: "documentation",
+    sidebarProps: {
+      displayText: "Documentation",
+      icon: <ArticleOutlinedIcon />
+    }
+  },
+  {
+    path: "/changelog",
+    element: <ChangelogPage />,
+    state: "changelog",
+    sidebarProps: {
+      displayText: "Changelog",
+      icon: <ListOutlinedIcon />
+    }
   }
 ]
 
